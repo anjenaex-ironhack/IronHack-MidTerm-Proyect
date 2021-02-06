db1225f90e933a37acfd2aa00ebc9cf28352be08
anjenaex <anjenaex@gmail.com>
2021-02-06 09:19:07 +0100

    first commit

---
+++ src/main/java/com/ironhack/Midtem/Project/MidtemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.Midtem.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtemProjectApplication.class, args);
+	}
+
+}
