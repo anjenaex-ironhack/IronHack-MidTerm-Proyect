d089ab508d71d279f87003392fe19f453e523387
anjenaex <anjenaex@gmail.com>
2021-02-12 06:15:58 +0100

    SavingDTO created

--- src/main/java/com/ironhack/Midtem/Project/controller/dto/SavingDTO.java
+++ src/main/java/com/ironhack/Midtem/Project/controller/dto/SavingDTO.java
@@ -13,20 +13,75 @@ public class SavingDTO extends AccountDTO{
 
     @NotEmpty
     private String secretKey;
-    @DecimalMin(value = "250", message = "The minimum balance when you create a checking account is 250")
+    @DecimalMin(value = "100", message = "The minimum balance when you create a saving account is 100")
     private BigDecimal balanceAmount;
     private Currency balanceCurrency;
-    @DecimalMin(value = "250", message = "The minimum balance when you create a checking account is 250")
-    private BigDecimal minimumBalanceAmount;
+    @DecimalMax(value = "1000", message = "The maximum balance when you create a saving account is 1000")
+    @DecimalMin(value = "100", message = "The minimum balance when you create a saving account is 100")
+    private BigDecimal minimumBalanceAmount = new BigDecimal("1000");
     private Currency minimumBalanceCurrency;
-    @DecimalMin(value = "12", message = "the monthly maintenance fee has to be 12")
-    @DecimalMax(value = "12", message = "the monthly maintenance fee has to be 12")
-    private BigDecimal monthlyMaintenanceFeeAmount = new BigDecimal("12");
-    private Currency monthlyMaintenanceFeeCurrency;
+    @DecimalMax(value = "0.5", message = "The maximum interest rate when you create a saving account is 0.5")
+    private BigDecimal interestRate = new BigDecimal("0.0025");
     private Status status = Status.ACTIVE;
 
 
     public SavingDTO(AccountHolder primaryOwner, AccountHolder secondaryOwner) {
         super(primaryOwner, secondaryOwner);
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public BigDecimal getBalanceAmount() {
+        return balanceAmount;
+    }
+
+    public void setBalanceAmount(BigDecimal balanceAmount) {
+        this.balanceAmount = balanceAmount;
+    }
+
+    public Currency getBalanceCurrency() {
+        return balanceCurrency;
+    }
+
+    public void setBalanceCurrency(Currency balanceCurrency) {
+        this.balanceCurrency = balanceCurrency;
+    }
+
+    public BigDecimal getMinimumBalanceAmount() {
+        return minimumBalanceAmount;
+    }
+
+    public void setMinimumBalanceAmount(BigDecimal minimumBalanceAmount) {
+        this.minimumBalanceAmount = minimumBalanceAmount;
+    }
+
+    public Currency getMinimumBalanceCurrency() {
+        return minimumBalanceCurrency;
+    }
+
+    public void setMinimumBalanceCurrency(Currency minimumBalanceCurrency) {
+        this.minimumBalanceCurrency = minimumBalanceCurrency;
+    }
+
+    public BigDecimal getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(BigDecimal interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
